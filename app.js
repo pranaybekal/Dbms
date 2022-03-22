2e47d0e38f1062abdaac81d22eabc285cb254c10
Udith Bekal <udithbkl100@gmail.com~>
2022-03-21 17:52:33 -0700

    initial committ

--- app.js
+++ app.js
@@ -96,10 +96,6 @@ app.use('/img',img)
 
 // ------------------------------------------------------------------------------------------------------------
 // listen  op
-// app.listen(3000,()=>
-// {
-//     console.log("running at 3000");
-// });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
