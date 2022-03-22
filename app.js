2e89e5c09164748c251cf3f007bab71a4ec3d441
Udith Bekal <udithbkl100@gmail.com~>
2022-03-21 17:48:40 -0700

    initial committ

--- app.js
+++ app.js
@@ -100,6 +100,7 @@ app.use('/img',img)
 // {
 //     console.log("running at 3000");
 // });
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
